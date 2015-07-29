535181a7643c4de637068964fd410c3c5e5bb229
Eshin Jolly <ejolly@users.noreply.github.com>
2015-07-28 21:21:07 -0400

    Finished player matching. Some options for routing players to game.

--- client/js/lobby.js
+++ client/js/lobby.js
@@ -1,10 +1,27 @@
 Template.lobby.helpers({
   players:function(){
-    return Players.find({status:"waiting"},{sort:{enterTime:-1}});
+    return Players.find({status:'waiting'},{sort:{enterTime:-1}});
   },
   countPlayers:function(){ 
-  	return Players.find({status:"waiting"},{}).count();
+  	return Players.find({status:'waiting'},{}).count();
+  },
+  //Return the status of the current client
+  
+  status:function(){
+    var currentUser = Meteor.userId();
+    return Players.find({"_id" :currentUser}, {fields:{"status":1}});
+    //return Players.find({"_id" :currentUser}).fetch()[0].status;
   }
+    
+
+    /*if (currentUserStatus === 'waiting'){
+      return true;
+    } else if (typeof currentUserStatus === 'undefined'){
+      return true;
+    } else{
+      return false;
+    }
+  } */
 });
 
 Template.lobby.onRendered(function(){
