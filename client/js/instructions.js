9d13ec361b641939dabbcb41b70097d12ca36c59
Eshin Jolly <ejolly@users.noreply.github.com>
2015-08-04 13:19:25 -0400

    Minor code cleanup

--- client/js/instructions.js
+++ client/js/instructions.js
@@ -1,24 +1,3 @@
-/* Template.instructions.helpers({
-  
-  players:function(){
-    /*Initially queries db for waiting players. If client is matched and not in lobby, then routes to instructions. Also routes to instructions if no one is in the lobby (empty db query) 
-    var currentUser = Meteor.userId();
-    var lobbyUsers = Players.find({status:'waiting'},{sort:{enterTime:1}});
-    if(jQuery.isEmptyObject(lobbyUsers.fetch()[0]) || currentUser != lobbyUsers.fetch()[0]._id){
-      Router.go('instructions');
-
-    } else {
-       return lobbyUsers;
-    } 
-  },
-
-  countPlayers:function(){ 
-  	return Players.find({status:'waiting'},{}).count();
-  } 
-}); 
-*/
-
-
 Template.instructions.events({
 
   'click .ready-to-start': function(event){
