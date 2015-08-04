cb8ada9aa3f4c72c5e54a1d79cd259614603eb4d
Eshin Jolly <ejolly@users.noreply.github.com>
2015-08-03 21:13:11 -0400

    Got ready button to game tree routing working.

--- client/js/instructions.js
+++ client/js/instructions.js
@@ -1,7 +1,7 @@
-Template.instructions.helpers({
+/* Template.instructions.helpers({
   
   players:function(){
-    /* Initially queries db for waiting players. If client is matched and not in lobby, then routes to instructions. Also routes to instructions if no one is in the lobby (empty db query) */
+    /*Initially queries db for waiting players. If client is matched and not in lobby, then routes to instructions. Also routes to instructions if no one is in the lobby (empty db query) 
     var currentUser = Meteor.userId();
     var lobbyUsers = Players.find({status:'waiting'},{sort:{enterTime:1}});
     if(jQuery.isEmptyObject(lobbyUsers.fetch()[0]) || currentUser != lobbyUsers.fetch()[0]._id){
@@ -15,13 +15,16 @@ Template.instructions.helpers({
   countPlayers:function(){ 
   	return Players.find({status:'waiting'},{}).count();
   } 
-});
+}); 
+*/
+
 
 Template.instructions.events({
 
   'click .ready-to-start': function(event){
     var gameId = Games.find().fetch()[0]._id;
     Meteor.call('playerReady',gameId);
+
   }
 
 });
