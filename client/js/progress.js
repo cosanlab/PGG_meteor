6195f7ff8f585877c6c418bb769330a982bd7b73
Eshin Jolly <ejolly@users.noreply.github.com>
2015-08-11 18:07:02 -0400

    Made progress bar code more meteoric. Cleaned up some uneccesary code.

--- client/js/progress.js
+++ client/js/progress.js
@@ -1,9 +1,47 @@
 Template.progress.helpers({
 	game: function(){
 		var game= Games.findOne();
-		return {
-			state: game.state,
-			condition: game.condition
-		};
+		if(!game){
+			return false;
+		} else{ 
+			var instr = ""; 
+			var mess = "";
+			var pA = "";
+			var pB = "";
+			var over = ""; 
+			var summ = "";
+
+			switch(game.state){
+				case 'instructions':
+					instr = "active";
+					break;
+				case 'playerBmessaging':
+					mess = 'active';
+					break;
+				case 'playerAdeciding':
+					pA = 'active';
+					break;
+				case 'playerBdeciding':
+					pB = 'active';
+					break;
+				case 'finalChoices':
+					over = 'active';
+					break;
+				case 'ended':
+					summ = 'active';
+					break;
+				}
+			return {
+				state: game.state,
+				condition: game.condition,
+				instr: instr,
+				mess: mess,
+				pA: pA,
+				pB: pB,
+				over: over,
+				summ: summ
+			};
+		}
 	}
 });
+
