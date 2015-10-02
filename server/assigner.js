389a30dd68856cc349b1c0aa4d03d19971e4204c
Eshin Jolly <ejolly@users.noreply.github.com>
2015-10-02 14:48:56 -0400

    Broke server files into separate parts. Updated assigner with group size and lobby timer. Implemented game creation.

--- server/assigner.js
+++ server/assigner.js
@@ -3,54 +3,63 @@ var extend = function(child, parent) { for (var key in parent) { if (hasProp.cal
   hasProp = {}.hasOwnProperty;
 
 //Our assigner which takes in a group size and handles making sure new users are put into a game with another person or otherwise sent to the exit survey 
-TurkServer.Assigners.UGAssigner = (function(superClass) {
-  extend(UGAssigner, superClass);
+TurkServer.Assigners.PGGAssigner = (function(superClass) {
+  extend(PGGAssigner, superClass);
 
-  function UGAssigner(groupSize){
+  function PGGAssigner(groupSize,treatment){
     this.groupSize = groupSize;
-    //return UGAssigner.__super__constructor.apply(this,arguments);
+    this.treatment = treatment;
+    this.lobbyTimers = [];
   }
 
-  UGAssigner.prototype.initialize = function(){
+  PGGAssigner.prototype.initialize = function(){
     //Call the parent's initialize method first
-    UGAssigner.__super__.initialize.apply(this, arguments);
+    PGGAssigner.__super__.initialize.apply(this, arguments);
   };
 
-  UGAssigner.prototype.userJoined = function(asst){
+  PGGAssigner.prototype.userJoined = function(asst){
     //If a user has been in an experiment before check to see if they need to be rematched, if so leave them in the lobby otherwise take them to the exit survey
+    //When leaving users in the lobby also start and store a 5 minute lobby timers
+    var asstId = asst.userId;
     if(asst.getInstances().length > 0){
-      if(!Players.findOne(asst.userId).needRematch){
-        this.lobby.pluckUsers([asst.userId]);
+      if(!Players.findOne(asstId).needRematch){
+        this.lobby.pluckUsers([asstId]);
         return asst.showExitSurvey();
       }
-    } else if(!Players.findOne(asst.userId)){
-        Meteor.call('addPlayer', asst.userId);
+    } else if(!Players.findOne(asstId)){
+        Meteor.call('addPlayer', asstId);
+        var userlobbyBomb = Meteor.setTimeout(function(){
+          Meteor.call('lobbyTimeBomb',asstId);
+    },300000);
+        this.lobbyTimers.push({asstId:userLobbyBomb});
+
     }
 
-    //If there are 2 people in the lobby create a new instance and send both players there
+    //If there are 5 people in the lobby create a new instance and send both players there
     //Also change their status in the players db and create a game
+    //Also clear their lobby timers 
     var lobbyUsers = this.lobby.getAssignments();
     
     if (lobbyUsers.length == this.groupSize)  {
-      var treatment = _.sample(this.batch.getTreatments());
-      this.instance = this.batch.createInstance([treatment]);
+      this.instance = this.batch.createInstance([this.treatment]);
       this.instance.setup();
       results = []; playerIds = [];
       for (i = 0, len = lobbyUsers.length; i < len; i++) {
         asst = lobbyUsers[i];
+        Meteor.clearTimeout(this.lobbyTimers[i].asst.userId);
         this.lobby.pluckUsers([asst.userId]);
         Meteor.call('updatePlayerState', asst.userId,'instructions');
         results.push(this.instance.addAssignment(asst));
         playerIds.push(asst.userId);
       }
-      Meteor.call('createGame', playerIds, treatment);
+      Meteor.call('createGame',this.instance._id, playerIds, treatment);
       return results;
     } else{
       return; //do nothing cause there's not enough players
     }
   };
 
-  return UGAssigner;
+  return PGGAssigner;
 
 })(TurkServer.Assigner);
 
---
+++ server/startupAndPubs.js
@@ -0,0 +1,37 @@
+Meteor.startup(function(){
+	try{
+		//Create a test batch for now and give it an assigner
+		Batches.upsert({name: 'Test_1'}, {name: 'Test_1', active: true});
+		TurkServer.ensureTreatmentExists({name: 'withMessaging'});
+    	TurkServer.ensureTreatmentExists({name: 'noMessaging'});
+		Batches.update({name: 'Test_1'}, {$addToSet: {treatments: 'withMessaging'}});
+		Batches.update({name: 'Test_1'}, {$addToSet: {treatments: 'noMessaging'}});
+		//var batch = TurkServer.Batch.getBatchByName('Test_1');
+		//batch.setAssigner(new TurkServer.Assigners.UGAssigner(2));
+		 Batches.find().forEach(function(batch) {
+		 	TurkServer.Batch.getBatch(batch._id).setAssigner(new TurkServer.Assigners.UGAssigner(2));
+    		});
+	} 
+	catch(e){
+		console.log(e);
+		return;
+	}
+});
+
+///Subjects DB
+Meteor.publish('Players', function(){
+	return Players.find();
+});
+
+//Games DB
+Meteor.publish('Games', function(){
+   return Games.find();
+    // Security
+    //var currentUser = this.userId;		
+	//return Games.find({"$or": [{"playerA":currentUser},{"playerB":currentUser}]});
+});
+
+//Users for disconnection
+Meteor.publish('userStatus',function(){
+	return Meteor.users.find({"status.online":true});
+});
