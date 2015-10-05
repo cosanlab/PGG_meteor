24ae3d3ffac09b59e41b031f5cd2ba2c3bced79c
Eshin Jolly <Esh@dhcp-10-31-127-86.wireless.dartmouth.edu>
2015-10-05 17:53:51 -0400

    More assigner logic and lobby logic, based on player status.

--- client/js/client.js
+++ client/js/client.js
@@ -9,7 +9,7 @@ Meteor.startup(function(){
                     }
                 var batch = TurkServer.batch();
                 Meteor.subscribe('lobby', batch && batch._id);
-                Router.go('lobbyUG');
+                Router.go('lobby');
             }
             //When a user is in an experiment, if their partner has disconnected show them the disconnection template and start a timer that will eventually end the experiment. Otherwise, clear any experiment-ending timers (e.g. if everyone reconnects) and show them the instructions template which has it's own logic about which state of the game they should see
             else if (TurkServer.inExperiment()){
@@ -46,22 +46,6 @@ Tracker.autorun(function(){
 });
 
 
-/*
-//When a player joins a game start a reactive computation monitoring their partner's connection status
-if(Players.findOne(Meteor.userId()).status != 'waiting'){
-    Tracker.autorun(function(){
-
-
-    });
-
-}
-//Keep tabs on the other user being played with
-
-Tracker.autorun(function(){
-    var group = Turkserver.group();
-});
-*/
-
 //New Spacebars function that should work on all templates
 Template.registerHelper("equals", function(a,b){
     return (a==b);
@@ -70,92 +54,3 @@ Template.registerHelper("notequals", function(a,b){
     return (a!=b);
 });
 
-// Login validation
-$.validator.setDefaults({
-    rules: {
-        email: {
-            required: true,
-            email: true
-        },
-        password: {
-            required: true,
-            minlength: 6
-        }
-    },
-    messages: {
-        email: {
-            required: "You must enter an email address.",
-            email: "You've entered an invalid email address."
-        },
-        password: {
-            required: "You must enter a password.",
-            minlength: "Your password must be at least {0} characters."
-        }
-    }
-});
-
-/*
-//Registration handler
-Template.register.onRendered(function(){
-    var validator = $('.register').validate({
-        submitHandler: function(event){
-            var email = $('[name=email]').val();
-            var password = $('[name=password]').val();
-            Accounts.createUser({
-                email: email,
-                password: password
-            }, function(error){
-                if(error){
-                    if(error.reason == "Email already exists."){
-                        validator.showErrors({
-                            email: "That email already belongs to a registered user."   
-                        });
-                    }
-                } else {
-                     Meteor.call('addPlayer', {}, function(){
-                        Router.go('lobby');
-                });
-                }   
-            });
-        }
-    });
-});
-
-*/
-//Login handler
-/*
-Template.login.onRendered(function(){
-    var validator = $('.login').validate({
-        submitHandler: function(event){
-            var email = $('[name=email]').val();
-            var password = $('[name=password]').val();
-            Meteor.loginWithPassword(email, password, function(error){
-              if(error){
-                  if(error.reason == "User not found"){
-                      validator.showErrors({
-                        email: "That email doesn't belong to a registered user."   
-                      });
-                  }
-                  if(error.reason == "Incorrect password"){
-                      validator.showErrors({
-                        password: "You entered an incorrect password."    
-                      });
-                  }
-              } else {
-                Meteor.call('addPlayer', {}, function(){
-                    Router.go('lobby');
-                });
-              }
-            });
-        }
-    });
-}); */
-/*
-Template.navigation.events({
-    'click .logout': function(event){
-        event.preventDefault();
-        Meteor.logout();
-        Router.go('login');
-    }
-});
-*/
