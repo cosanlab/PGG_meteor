c4f72ce3503a13171e47dd8b1d4674e852885173
Eshin Jolly <ejolly@users.noreply.github.com>
2015-10-06 14:55:18 -0400

    Added possible logic for matching after client comprehension quiz while still enabling lobby timer.

--- server/playerMethods.js
+++ server/playerMethods.js
@@ -7,10 +7,26 @@ Meteor.methods({
 			enterTime: new Date(),
 			status: 'waiting',
 			quizAttempts: 0,
-			passedQuiz: false,
 			needRematch: false,
 		};
 		Players.insert(data);
+	},
+	'updateQuiz': function(currentUser,result){
+		//Updates the Assignments db with a boolean about whether user passed the comprehension quiz. If so tells the assigner to try and match lobby users, otherwise shows them the exit survey 
+		var asst = TurkServer.Assignment.getCurrentUserAssignment(currentUser);
+		var asstId = asst.asstId;
+		Assignments.update(asstId,{$set:{'passedQuiz':result}});
+		//Trigter match-players lobby event here maybe:
+		if(result){
+			var userlobbyBomb = Meteor.setTimeout(function(){
+          		Meteor.call('lobbyTimeBomb',asstId);
+        		},300000);
+        	TurkServer.PGGAssigner.lobbyTimers.push({currentUser:userLobbyBomb});
+        	return TurkServer.Lobby.events.emit('match-players');
+      	} else{
+      		Meteor.call('updatePlayerInfo',currentUser,{'status':'failedQuiz'});
+      		asst.showExitSurvey();
+      	}	
 	},
 	//Remove a player document from the database
 	'removePlayer': function(currentUser){
@@ -22,8 +38,10 @@ Meteor.methods({
 
 	},
 	//Lobby timer so players don't wait too long to get match
-	lobbyTimeBob: function(currentUser){
+	lobbyTimeBomb: function(currentUser){
 		asst = TurkServer.Assignment.getCurrentUserAssignment(currentUser);
-		ass.showExitSurvey();
+		var asstId = asst.asstId;
+		Meteor.call('updatePlayerInfo',asstId,{'status':'lobbyTimeout'});
+		asst.showExitSurvey();
 	}
 });
