4c03ca0f64cf8e622e20cadf0e264fec375135f3
Eshin Jolly <Esh@dhcp-10-31-119-228.wireless.dartmouth.edu>
2015-11-11 18:35:56 -0500

    Current Version being depolyed.Updated disconnection handling for various game states. Created single disconnection bomb for each game. Increased lobby timer.

--- client/js/userDisconnect.js
+++ client/js/userDisconnect.js
@@ -2,14 +2,37 @@
 Template.userDisconnect.helpers({
     gameTooEarly: function(){
         var game = Games.findOne();
-        if (game.round == 1 && (game.state == 'pChoose' || game.state == 'assignment')){
+        var tooEarly = _.any(_.pluck(game.players,'contributions',_.isEmpty));
+        if (game.round == 1 && tooEarly){
             return true;
         } else {
             return false;
         }
     },
+    disconnectPay: function(){
+        return disconnectPay;
+    },
     disconnectTimeout: function (){
         return disconnectTimeout;
     },
+    clock: function(){
+    Meteor.setInterval(function(){
+      var sTime = Session.get('sTime');
+      var elapsed = (new Date() - sTime)/1000;
+      var m = Math.floor(elapsed/60);
+      var s = Math.floor(elapsed % 60);
+      s = s < 10 ? "0" + s: s;
+      Session.set('min',m);
+      Session.set('sec',s);
+    },900);
 
+    return {
+      min: Session.get('min'),
+      sec: Session.get('sec')
+    };
+  }
+});
+
+Template.userDisconnect.onCreated(function(){
+  Session.set('sTime',new Date());
 });
